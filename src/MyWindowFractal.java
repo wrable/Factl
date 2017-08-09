cf3813c0cd101ed909fbbcfbcd7d64bcd3cd1e2d
kwroblewski <stormpl@gmail.com>
2017-08-09 17:13:29 +0200

    cos tu strasznie pojebalem

--- src/MyWindowFractal.java
+++ src/MyWindowFractal.java
@@ -8,7 +8,6 @@ import javax.swing.JTextField;
 
 
 import seph.wrable.fractal.GenFractal;
-import seph.wrable.fractal.DrawFractal;
 
 public class MyWindowFractal extends JFrame implements ActionListener {
 
@@ -17,43 +16,42 @@ public class MyWindowFractal extends JFrame implements ActionListener {
 	JTextField tWprowadzAksjomat, tWprowadzRegule,tWyœwietlLSystem;
 
 	static GenFractal Fractal;
-	static DrawFractal rysuj;
 
 	public MyWindowFractal() {
 		setSize(500, 400);
 		setTitle("Generator Fractali");
 		setLayout(null); // layout menager
 
-//		bGenerujLSystem = new JButton("Generuj L-System");
-//		bGenerujLSystem.setBounds(100, 130, 300, 20); // pozycja rozmiar
-//		add(bGenerujLSystem); // dodawanie komponetu do panelu
-//		bGenerujLSystem.addActionListener(this); // dodanie action listenera
-//
-//		bWyjscie = new JButton("Wyjscie");
-//		bWyjscie.setBounds(100, 330, 300, 20); // pozycja rozmiar
-//		add(bWyjscie);
-//		bWyjscie.addActionListener(this);
-//
-//		tWyœwietlLSystem = new JTextField("Wynik");
-//		tWyœwietlLSystem.setBounds(100, 170, 300, 20);
-//		add(tWyœwietlLSystem);
-//
-//		lRegula = new JLabel("Regu³a [F: ]");
-//		lRegula.setBounds(100, 80, 300, 20);
-//		add(lRegula);
-//
-//		lAksjomat = new JLabel("Aksjomat [F]");
-//		lAksjomat.setBounds(100, 30, 300, 20);
-//		add(lAksjomat);
-//
-//		tWprowadzAksjomat = new JTextField("F");
-//		tWprowadzAksjomat.setBounds(100, 50, 300, 20);
-//		add(tWprowadzAksjomat);
-//
-//		tWprowadzRegule = new JTextField("FF");
-//		tWprowadzRegule.setBounds(100, 100, 300, 20);
-//		add(tWprowadzRegule);
-		rysuj = new DrawFractal();
+		bGenerujLSystem = new JButton("Generuj L-System");
+		bGenerujLSystem.setBounds(100, 130, 300, 20); // pozycja rozmiar
+		add(bGenerujLSystem); // dodawanie komponetu do panelu
+		bGenerujLSystem.addActionListener(this); // dodanie action listenera
+
+		bWyjscie = new JButton("Wyjscie");
+		bWyjscie.setBounds(100, 330, 300, 20); // pozycja rozmiar
+		add(bWyjscie);
+		bWyjscie.addActionListener(this);
+
+		tWyœwietlLSystem = new JTextField("Wynik");
+		tWyœwietlLSystem.setBounds(100, 170, 300, 20);
+		add(tWyœwietlLSystem);
+
+		lRegula = new JLabel("Regu³a [F: ]");
+		lRegula.setBounds(100, 80, 300, 20);
+		add(lRegula);
+
+		lAksjomat = new JLabel("Aksjomat [F]");
+		lAksjomat.setBounds(100, 30, 300, 20);
+		add(lAksjomat);
+
+		tWprowadzAksjomat = new JTextField("F+F+F");
+		tWprowadzAksjomat.setBounds(100, 50, 300, 20);
+		add(tWprowadzAksjomat);
+
+		tWprowadzRegule = new JTextField("F+F-F-F+F");
+		tWprowadzRegule.setBounds(100, 100, 300, 20);
+		add(tWprowadzRegule);
+
 		
 	}
 
@@ -62,7 +60,6 @@ public class MyWindowFractal extends JFrame implements ActionListener {
 		newWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // domyœlna reakcja na x
 		newWindow.setVisible(true); // visible true
 		Fractal = new GenFractal();
-		newWindow.add(rysuj);
 		
 
 
